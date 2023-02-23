ca367496f4ed1cecf64b241f251c6fc8f0822f5f
kihyeon Kim <113660408+ppspai@users.noreply.github.com>
2023-02-23 14:48:32 +0900

    찐막

--- controllers/rankController.js
+++ controllers/rankController.js
@@ -3,7 +3,7 @@ const rankService = require('../services/rankService');
 const rankRecipe = async (req, res) => {
   const { order } = req.query;
   const result = await rankService.rankRecipe(order);
-  res.json({message : result});
+  res.json({data : result});
 }
 
 module.exports = {
--- services/recipeService.js
+++ services/recipeService.js
@@ -1,4 +1,5 @@
 const recipeDAO = require('../models/recipeDAO');
+const jwt = require('jsonwebtoken');
 
 const getAllRecipe = async () => {
   const result = await recipeDAO.getAllRecipe();
@@ -12,11 +13,15 @@ const getRecipeById = async (id) => {
 }
 
 const createRecipe = async (token, title, description, amount, spend_time, level, thumbnail, ingredients, step) => {
-  
-  const user = jwt.verify(token, process.env.SECRET_KEY);
-  console.log(user);
+  if(!token) {
+    throw {
+      message : "로그인 후 가능합니다.",
+      status : 400,
+    }
+  }
+  const writer = jwt.verify(token, process.env.SECRET_KEY);
 
-  await recipeDAO.createRecipe(writer, title, description, amount, spend_time, level, thumbnail, ingredients, step);
+  await recipeDAO.createRecipe(writer.id, title, description, amount, spend_time, level, thumbnail, ingredients, step);
 }
 
 module.exports = {
